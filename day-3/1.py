ca26d691a296b923af3e4823e1b0ecfbd4233491
Tim Lolkema <mail@timlolkema.nl>
2024-12-03 15:33:30 +0100

    December 3

---
+++ day-3/1.py
@@ -0,0 +1,21 @@
+import re
+
+
+def main():
+    with open("./day-3/input-full.txt") as file:
+        content = file.read()
+
+        matches = re.findall(r"mul\(\d+,\d+\)", content)
+        clean_matches = [
+            match.replace("mul(", "").replace(")", "") for match in matches
+        ]
+
+        multiplications = [
+            int(match.split(",")[0]) * int(match.split(",")[1])
+            for match in clean_matches
+        ]
+        print(sum(multiplications))
+
+
+if __name__ == "__main__":
+    main()
